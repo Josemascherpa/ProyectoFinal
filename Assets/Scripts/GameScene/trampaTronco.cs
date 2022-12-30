44a79600d0d4164f98e7dadef59ad795ad3c018b
Josemascherpa <joseimascherpa@gmail.com>
2022-12-29 21:22:42 -0300

    Arreglado bug particulas

--- Assets/Scripts/GameScene/trampaTronco.cs
+++ Assets/Scripts/GameScene/trampaTronco.cs
@@ -24,7 +24,8 @@ public class trampaTronco : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {        
         if (collision.gameObject.CompareTag("tree") || collision.gameObject.CompareTag("NextLevel"))
-        {           
+        {
+            
             target = new Vector3(Random.Range(X.transform.position.x, Xn.transform.position.x), this.transform.position.y, Random.Range(Z.transform.position.z, Zn.transform.position.z));
         }
     }
