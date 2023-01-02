2995ee6e9304cf7248413db7c3f9ac2c0ef951be
Josemascherpa <joseimascherpa@gmail.com>
2023-01-02 12:08:17 -0300

    Cambiado de particulas de flechas

--- Assets/Scripts/GameScene/Arrow.cs
+++ Assets/Scripts/GameScene/Arrow.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    
+    private float timeDestroy = 2f;
 
     private void OnCollisionEnter(Collision collision)//Destruir al chocar algo
     {
-        Destroy(gameObject);
+        this.GetComponent<MeshRenderer>().enabled = false;
+        this.transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
+        StartCoroutine(destroyParticle());
     }
 
+    public IEnumerator destroyParticle()
+    {
+        yield return new WaitForSeconds(timeDestroy);
+        Destroy(this.gameObject);
+    }
 }
